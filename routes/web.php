72e418dede08923078886e55bdace0a35e9b216a
rumyTM <rumywithy@gmail.com>
2021-09-13 22:40:53 +0600

    Added required routes and logics

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +23,13 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/profiles/{user}', function (User $user) {
+    return view('profile', compact('user'));
+})->name('profiles.show');
+
+Route::get('/qrcodes/{user}', function (User $user) {
+    return Response::download(public_path('qrcodes/' . $user->qrcode), Str::random(15) . '.png');
+})->name('qrcodes.download');
 
 require __DIR__.'/auth.php';
